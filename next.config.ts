f5b1283038de71556464ccde59a2a1f045fc89b9
HÔKWÈ Holding <hokweholding@gmail.com>
2025-01-03 18:54:07 +0100

    Commti

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  reactStrictMode: true, // Enables React strict mode
+  //swcMinify: true, // Uses SWC for faster builds and minification
+  images: {
+    domains: ["hokwe.com"], // Allow images from specific domains
+  },
+  async redirects() {
+    return [
+      {
+        source: "/home",
+        destination: "/",
+        permanent: true, // Redirects /home to /
+      },
+    ];
+  },
 };
 
 export default nextConfig;
