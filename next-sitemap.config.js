1386e10c4dafb939ba11e2646c69cb5c14a00201
HÔKWÈ Holding <hokweholding@gmail.com>
2025-01-03 21:35:14 +0100

    Commti

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,5 +4,7 @@ module.exports = {
     generateRobotsTxt: true, // Generates a robots.txt file
     changefreq: 'daily', // Frequency of content changes
     priority: 0.7, // Default priority for URLs
+    sitemapSize: 5000, // Optional: Set max number of URLs per sitemap
+    outDir: './public',       // Directory to output the generated files  
   };
   
